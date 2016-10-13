56dc13c66fe15ce866a930ec56d443598cc8361e
mcgurk <lehtinen@sci.fi>
2016-10-13 19:15:40 +0300

    Update Stinger_WiiClassicController.ino

--- Stinger_WiiClassicController.ino
+++ Stinger_WiiClassicController.ino
@@ -2,14 +2,66 @@
 //Pro Micro: SDA = D2, SCL = D3
 //ESP8266: SDA = D2, SCL = D1
 
-//#define DEBUG
-
-#define LED 2
+//#define LED 2
 
 #include<Wire.h>
 
 #define JOYSTICK_DATA_SIZE 6
 #define JOYSTICK_STATE_SIZE 4
+
+//http://wiibrew.org/wiki/Wiimote/Extension_Controllers/Classic_Controller
+#define BDU !bitRead(data[5], 0) /* up */
+#define BDD !bitRead(data[4], 6) /* down */
+#define BDL !bitRead(data[5], 1) /* left */
+#define BDR !bitRead(data[4], 7) /* right */
+#define BST !bitRead(data[4], 2) /* start */
+#define BSE !bitRead(data[4], 4) /* select */
+#define BA !bitRead(data[5], 4) /* A */
+#define BB !bitRead(data[5], 6) /* B */
+#define BX !bitRead(data[5], 3) /* X */
+#define BY !bitRead(data[5], 5) /* Y */
+#define BLS !bitRead(data[5], 7) /* left shoulder */
+#define BRS !bitRead(data[5], 2) /* right shoulder */  
+#define BLT !bitRead(data[4], 5) /* left trigger */
+#define BRT !bitRead(data[4], 1) /* right trigger */
+//RT = (status[3] & (unsigned char)0x1f); //rightmost 5 bits
+//LT = ((status[2] & (unsigned char)0x60) >> 2) + ((status[3] & (unsigned char)0xe0) >> 5); //'leftish' bits
+//leftStickX = (status[0] & (unsigned char)0x3f); //rightmost 6 bits
+//leftStickY = (status[1] & (unsigned char)0x3f); //rightmost 6 bits      
+//rightStickX = ((status[0] & (unsigned char)0xc0) >> 3) + ((status[1] & (unsigned char)0xc0) >> 5) +  ((status[2] & (unsigned char)0x80) >> 7); //bits. They're there.
+//rightStickY = (status[2] & (unsigned char)0x1f);    //rightmost 5 bits  
+    
+inline void translateState(uint8_t *data, uint8_t *state) {
+  //https://github.com/torvalds/linux/blob/master/drivers/input/joystick/stinger.c
+  state[0] = 0; state[1] = 0; state[2] = 0; state[3] = 0;
+  if (BA) bitSet(state[0], 5);  //a 
+  if (BB) bitSet(state[0], 4);  //b
+  if (BST) bitSet(state[3], 1);  //select
+  if (BSE) bitSet(state[3], 0);  //start
+  if (BX) bitSet(state[0], 2);  //x
+  if (BY) bitSet(state[3], 5);  //y
+  if (BLT) bitSet(state[3], 3); //tl
+  if (BRT) bitSet(state[3], 2); //tr
+  if (BLS) bitSet(state[0], 3); //c
+  if (BRS) bitSet(state[3], 4); //z
+  if (BDU) {
+    state[2] = B00111111; //-32767
+    bitClear(state[0], 1);
+  }  
+  if (BDD) {
+    state[2] = B00000000; //32767
+    bitSet(state[0], 1);
+  }
+  if (BDL) {
+    state[1] = B00000000; //-32767
+    bitSet(state[0], 0);
+  }
+  if (BDR) {
+    state[1] = B00111111; //32767
+    bitClear(state[0], 0);
+  }
+}
+
 
 #define ADDRESS 0x52
 
@@ -22,9 +74,9 @@ void sendByte(int data, int location)  {
 }
 
 void setup() {
-  Serial.begin(1200);
-  pinMode(LED, OUTPUT);
-  digitalWrite(LED, HIGH);
+  Serial.begin(9600);
+  //pinMode(LED, OUTPUT);
+  //digitalWrite(LED, HIGH);
   Wire.begin();
   sendByte(0x55, 0xF0);
   sendByte(0x00, 0xFB);
@@ -55,76 +107,13 @@ void loop() {
     delay(100);
   } else sendByte(0x00, 0x00);
 
-//https://github.com/torvalds/linux/blob/master/drivers/input/joystick/stinger.c
-//http://wiibrew.org/wiki/Wiimote/Extension_Controllers/Classic_Controller
-  uint8_t BDU, BDD, BDL, BDR, BA, BB, BM, BP, BX, BY, BLT, BRT, BZL, BZR;
-  BDU = !bitRead(data[5], 0);  //up
-  BDD = !bitRead(data[4], 6);  //down
-  BDL = !bitRead(data[5], 1);  //left
-  BDR = !bitRead(data[4], 7);  //right
-  BA = !bitRead(data[5], 4);  //A
-  BB = !bitRead(data[5], 6);  //B
-  BM = !bitRead(data[4], 4);  //-/select
-  BP = !bitRead(data[4], 2);  //+/start
-  BX = !bitRead(data[5], 3);  //X
-  BY = !bitRead(data[5], 5);  //Y
-  BLT = !bitRead(data[4], 5);  //LT
-  BRT = !bitRead(data[4], 1);  //RT
-  BZL = !bitRead(data[5], 7);  //ZL
-  BZR = !bitRead(data[5], 2);  //ZR
-  //RT = (status[3] & (unsigned char)0x1f); //rightmost 5 bits
-  //LT = ((status[2] & (unsigned char)0x60) >> 2) + ((status[3] & (unsigned char)0xe0) >> 5); //'leftish' bits
-  //leftStickX = (status[0] & (unsigned char)0x3f); //rightmost 6 bits
-  //leftStickY = (status[1] & (unsigned char)0x3f); //rightmost 6 bits      
-  //rightStickX = ((status[0] & (unsigned char)0xc0) >> 3) + ((status[1] & (unsigned char)0xc0) >> 5) +  ((status[2] & (unsigned char)0x80) >> 7); //bits. They're there.
-  //rightStickY = (status[2] & (unsigned char)0x1f);    //rightmost 5 bits  
-    
-  state[0] = 0; state[1] = 0; state[2] = 0; state[3] = 0;
-  if (BA) bitSet(state[0], 5);  //a 
-  if (BB) bitSet(state[0], 4);  //b
-  if (BM) bitSet(state[3], 1);  //select
-  if (BP) bitSet(state[3], 0);  //start
-  if (BX) bitSet(state[0], 2);  //x
-  if (BY) bitSet(state[3], 5);  //y
-  if (BLT) bitSet(state[3], 3); //tl
-  if (BRT) bitSet(state[3], 2); //tr
-  if (BZL) bitSet(state[0], 3); //c
-  if (BZR) bitSet(state[3], 4); //z
-  
-  //state[1] = 5; //585
-  //state[2] = 5; //-585
-  //state[1] = 0; //0
-  //state[2] = -5; //-32183
-  //state[1] = B00111111; //32767
-  //state[2] = B00100000; //-16384
-  //state[1] = B11000000; //0
-  //state[2] = B00111111; //-32767
-  //state[1] = B10000000; //0
-  if (BDU) {
-    state[2] = B00111111; //-32767
-    bitClear(state[0], 1);
-  }  
-  if (BDD) {
-    state[2] = B00000000; //32767
-    bitSet(state[0], 1);
-  }
-  if (BDL) {
-    state[1] = B00000000; //-32767
-    bitSet(state[0], 0);
-  }
-  if (BDR) {
-    state[1] = B00111111; //32767
-    bitClear(state[0], 0);
-  }
   
   if (memcmp(olddata, data, JOYSTICK_DATA_SIZE)) {    
     memcpy(olddata, data, JOYSTICK_DATA_SIZE);
-    Serial.write(state[0]);
-    Serial.write(state[1]);
-    Serial.write(state[2]);
-    Serial.write(state[3]);
-    Serial.flush();
-  } else delayMicroseconds(1000);
+    translateState(data, state);
+    Serial.write(state, 4);
+    //Serial.flush();
+  }
 
   const char *response = "\r\n0600520058C272";
   String s = "";
@@ -141,19 +130,14 @@ void loop() {
   }
 
   if (s == "E5E5") {
-    Serial.write(response);
-    digitalWrite(LED, LOW);
-    delay(2000);
-    digitalWrite(LED, HIGH);
+    Serial.write(response, 16);
+    sendByte(0x55, 0xF0);
+    sendByte(0x00, 0xFB);
+    //digitalWrite(LED, LOW);
+    //delay(2000);
+    //digitalWrite(LED, HIGH);
   }
 
-  //delay(1000);
+  delayMicroseconds(1000);
 }
-
-
-
-
-
-
-
 
